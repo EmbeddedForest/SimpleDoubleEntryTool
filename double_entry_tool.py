09135d5fc37d4b34a0746ceb4377ca7b691e28d8
TomDog <sticha.tom@gmail.com>
2026-01-17 22:40:06 -0600

    Add function to properly map import file columns

--- double_entry_tool.py
+++ double_entry_tool.py
@@ -27,52 +27,6 @@ from account_file import AccountFile
 from journal_file import JournalFile
 
 
-
-# def InitializationChecks(gui):
-#     ''' Check Accounts.csv and Journal.csv are legit '''
-
-#     # Check that 'Accounts.csv' file exists
-#     try:
-#         open(ACCOUNTS_FP, newline="", encoding="utf-8-sig")
-#     except:
-#         gui.Log('Accounts.csv does not exist in current directory.', 'error')
-
-#     # Check that 'Journal.csv' file exists
-#     try:
-#         open(JOURNAL_FP, newline="", encoding="utf-8-sig")
-#     except:
-#         gui.Log('Journal.csv does not exist in current directory.', 'error')
-
-
-#     # Check that 'Accounts.csv' file has necessary columns
-#     try:
-#         with open(ACCOUNTS_FP, newline="", encoding="utf-8-sig") as f:
-#             reader = csv.DictReader(f)
-#             headerList = list(next(reader).keys())
-
-#         if (set(headerList) != set(ACCT_HEADERS)):
-#             gui.Log('Accounts.csv is not syntactically correct.', 'error')
-
-#     except:
-#         gui.Log('Something bad happened', 'error')
-#         raise
-
-
-#     # Check that 'Journal.csv' file has necessary columns
-#     try:
-#         with open(JOURNAL_FP, newline="", encoding="utf-8-sig") as f:
-#             reader = csv.DictReader(f)
-#             headerList = list(next(reader).keys())
-
-#         if (set(headerList) != set(JRNL_HEADERS)):
-#             gui.Log('Journal.csv is not syntactically correct.', 'error')
-
-#     except:
-#         gui.Log('Something bad happened', 'error')
-#         raise
-
-
-
 def GetAllDataFileNames():
     ''' Returns all csv file names from Data folder as a list of strings '''
 
@@ -84,67 +38,6 @@ def GetAllDataFileNames():
             returnList.append(file)
 
     return returnList
-
-
-
-# def GetAllAccountNames(gui, fullName):
-#     ''' Returns account names as a list of strings '''
-
-#     returnList = list()
-
-#     if (fullName == True):
-#         colName = 'Full Account Name'
-#     else:
-#         colName = 'Account Name'
-
-#     try:
-#         with open(c.ACCOUNTS_FP, newline="", encoding="utf-8-sig") as f:
-#             reader = csv.DictReader(f)
-#             for row in reader:
-#                 returnList.append(row[colName])
-#     except:
-#         gui.Log('Something bad happened', 'error')
-#         raise
-
-#     return returnList
-
-
-
-# def CheckImportColmns(gui):
-#     ''' Checks to see if selected csv import file is legit '''
-
-#     # Check that csv file has necessary columns
-#     try:
-#         filePath = DATA_FOLDER + 'balls.csv'
-
-#         with open(filePath, newline="", encoding="utf-8-sig") as f:
-#             reader = csv.DictReader(f)
-#             headerList = list(next(reader).keys())
-
-#             tmp = any(head in headerList for head in ACCEPTED_DATE_NAMES)
-#             if (tmp == False):
-#                 gui.Log('Import csv does not have Date column.', 'error')
-#                 return BAD
-
-#             tmp = any(head in headerList for head in ACCEPTED_DESCRIPTION_NAMES)
-#             if (tmp == False):
-#                 gui.Log('Import csv does not have Description column.', 'error')
-#                 return BAD
-
-#             tmp = any(head in headerList for head in ACCEPTED_AMOUNT_NAMES)
-#             if (tmp == False):
-#                 gui.Log('Import csv does not have Amount column.', 'error')
-#                 return BAD
-
-#     except FileNotFoundError:
-#         gui.Log('Selected Import csv file does not exist', 'error')
-
-#     except:
-#         gui.Log('Something bad happened', 'error')
-#         raise
-
-#     return GOOD
-
 
 
 def ToolStart(gui, inFile, acctFile, journalFile):
@@ -161,12 +54,12 @@ def ToolStart(gui, inFile, acctFile, journalFile):
         gui.Log(msg)
         return
 
-    # # Map import csv columns
-    # retVal = MapImportColmns(gui)
-    # if (retVal == BAD):
-    #     return
-
-    # Map journal csv columns
+    # Setup import file object to point to correct columns
+    filePath = c.DATA_FOLDER + gui.selectedImportFile.get()
+    retVal, msg = inFile.MapColumns(filePath)
+    if (retVal == c.BAD):
+        gui.Log(msg)
+        return
 
     # Loop through all import transactions and compare against journal
 
--- import_file.py
+++ import_file.py
@@ -55,3 +55,55 @@ class ImportFile():
         # All good
         log = 'Import file is legit', 'default'
         return c.GOOD, log
+
+    def MapColumns(self, filePath):
+        ''' Connects to necessary headers in specified csv file '''
+
+        # Check that csv file exists
+        try:
+            with open(filePath, newline="", encoding="utf-8-sig") as f:
+                reader = csv.DictReader(f)
+                headerList = list(next(reader).keys())
+
+        except FileNotFoundError:
+            log = 'Selected Import csv file does not exist', 'error'
+            return c.BAD, log
+
+        except:
+            log = 'Something bad happened', 'error'
+            raise
+
+        self.dateCol = ' '
+        self.descriptionCol = ' '
+        self.amountCol = ' '
+
+        for dateName in c.ACCEPTED_DATE_NAMES:
+            if (dateName in headerList):
+                self.dateCol = dateName
+                break
+
+        for descriptionName in c.ACCEPTED_DESCRIPTION_NAMES:
+            if (descriptionName in headerList):
+                self.descriptionCol = descriptionName
+                break
+
+        for amountName in c.ACCEPTED_AMOUNT_NAMES:
+            if (amountName in headerList):
+                self.amountCol = amountName
+                break
+
+        if (self.dateCol == ' '):
+            log = 'Bad date column in import file', 'error'
+            return c.BAD, log
+
+        if (self.descriptionCol == ' '):
+            log = 'Bad description column in import file', 'error'
+            return c.BAD, log
+
+        if (self.amountCol == ' '):
+            log = 'Bad amount column in import file', 'error'
+            return c.BAD, log
+
+        # Looks good
+        log = 'Import columns mapped successfully'
+        return c.GOOD, log
